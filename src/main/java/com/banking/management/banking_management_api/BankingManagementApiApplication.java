afde88cff3da031606e368e7e2c4243fac74593d
Bruno Buttros <bruno.buttros@hotmail.com>
2025-01-08 21:18:53 -0300

    inicializando projeto

---
+++ src/main/java/com/banking/management/banking_management_api/BankingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.banking.management.banking_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/management/banking_management_api/BankingManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.management.banking_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
